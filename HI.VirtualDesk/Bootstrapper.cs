1124484f59e44863c5a363742c37b18119e3a162
george.andras <george.andras@yahoo.com>
2016-02-03 21:04:59 +0200

    Initial Version.

---
+++ HI.VirtualDesk/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using HI.VirtualDesk.Views;
+using System.Windows;
+
+namespace HI.VirtualDesk
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
